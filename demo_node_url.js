c9d8ba1120a0ad919d69d4df76b3183457f879b2
Amol <007amolp@gmail.com>
2020-08-22 19:10:43 +0530

    First commit

---
+++ demo_node_url.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs= require('fs');
+
+http.createServer(function(req,res){
+    fs.appendFile('web.html','hrojkkk',function(err){
+
+    });
+fs.readFile('web.html',function(err,data){
+res.writeHead(200,{'Content-type':'text/html'});
+res.write(data);
+return res.end();
+});
+}).listen(8080);
